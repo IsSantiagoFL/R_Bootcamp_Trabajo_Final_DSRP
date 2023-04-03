9e8a471b434c323688e02ffcead8a87c1dada81f
SantiagoIsmaelFlores-PCPl <santiagoflores@vivaldi.net>
2023-04-03 18:08:07 -0500

    Se creo el script analisis.R para analizar los datos de la base de datos games.csv y se importo la base de datos al script

---
+++ code/analisis.R
@@ -0,0 +1,18 @@
+################################################################################
+# Script: analisis.R
+# Autor: Santiago Ismael Flores Chávez
+# Fecha de creación: 3 de abril de 2023
+# Descripción: ...
+
+################################################################################
+
+# Carga de librerías necesarias
+
+
+################################################################################
+
+# Importando el dataset a R
+
+bd_games <- read.csv("data/games.csv", header = TRUE, sep = ",")
+
+
