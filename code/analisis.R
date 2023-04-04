c24dff4178611eaa804142ef61ba958c31cd044f
SantiagoIsmaelFlores-PCPl <santiagoflores@vivaldi.net>
2023-04-04 08:23:39 -0500

    tarea 1.4: Se sumo 5 al vector precio_final y se creo un nuevo vector, precio_final_aumentado

--- code/analisis.R
+++ code/analisis.R
@@ -15,21 +15,24 @@
 
 bd_games <- read.csv("data/games.csv", header = TRUE, sep = ",")
 
-# Creando vectores con los títulos del dataset
+# 1. Creando vectores con los títulos del dataset
 
 column_names <- names(bd_games)
 cat(column_names, sep = "\n")
 
-# Creando un vector numérico con el precio final de los vídeo juegos
+# 2. Creando un vector numérico con el precio final de los vídeo juegos
 
 precio_final <- bd_games$price_final
 class(precio_final)
 
-# Creando una condición lógica de precios bajos (ejemplo precios menores a 10.99) en el data frame original
+# 3. Creando una condición lógica de precios bajos (ejemplo precios menores a 10.99) en el data frame original
  
 precios_bajos <- precio_final < 10.99
 
 bd_games_precios_bajos <- bd_games[precios_bajos, ]
+
+# 4. Sumar 5 al vector creado: precio_final
+precio_final_aumentado <- precio_final + 5
 
 
 
